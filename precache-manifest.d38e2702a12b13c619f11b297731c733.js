6b55a7cc48e409e798aa544d8dada4eaed39d779
ashish kumar rena <ashishkumarrena@ashishs-MacBook-Air.local>
2020-05-17 13:44:48 +0530

    Updates

---
+++ precache-manifest.d38e2702a12b13c619f11b297731c733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894dd43d1fe4d5d096ec97723c45c12b",
+    "url": "/Bosch-AdminPanel/index.html"
+  },
+  {
+    "revision": "ded26f9a762405de017d",
+    "url": "/Bosch-AdminPanel/static/css/main.4afe3846.chunk.css"
+  },
+  {
+    "revision": "3f30538d86044118b0f6",
+    "url": "/Bosch-AdminPanel/static/js/2.4126cd53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Bosch-AdminPanel/static/js/2.4126cd53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded26f9a762405de017d",
+    "url": "/Bosch-AdminPanel/static/js/main.3dbface7.chunk.js"
+  },
+  {
+    "revision": "d8a32cde0ac713e4a911",
+    "url": "/Bosch-AdminPanel/static/js/runtime-main.f0422d6a.js"
+  }
+]);
